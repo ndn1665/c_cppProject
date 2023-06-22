5a935a012d93f11f716224bf3079559071fae377
ndn1665 <ndn1665@daum.net>
2023-06-23 01:18:31 +0900

    06.22 work

---
+++ Project2/Project2/operatoroverloading3.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#pragma warning(disable: 4996)
+
+using namespace std;
+using std::cin;
+
+class Point
+{
+private:
+	int xpos, ypos;
+public:
+	Point(int x = 0, int y = 0)
+		:xpos(x), ypos(y)
+	{}
+	//매개변수의 default값이 없으면 new Point[len]을 해줄 수 없다.
+	//왜냐하면 인자를 받지않는 void생성자의 호출을 통해서 배열요소를 이루는 객체가 생성되기 때문이다.
+	//따라서 void생성자가 호출됐을때 xpos와ypos에 값을 넣어줄 default값이 있어야한다.
+	friend ostream& operator<<(ostream& os, Point& pos);
+};
+
+ostream& operator<<(ostream& os, Point& pos)
+{
+	os << pos.xpos << "," << pos.ypos << endl;
+	return os;
+}
+typedef Point* Point_ptr;
+class BoundedArray
+{
+private:
+	Point_ptr* arr2;
+	//Point* arr;
+	int arrlen;
+public:
+	BoundedArray(int len)
+		:arrlen(len)
+	{
+		//	arr = new Point[len];
+		arr2 = new Point_ptr[len];
+	}
+	/*
+	Point& operator[](int idx)
+	{
+		if (idx < 0 || idx >= arrlen)
+		{
+			cout << "index out of range" << endl;
+			exit(1);
+		}
+		return arr[idx];
+	}
+	*/
+
+	Point_ptr& operator[](int idx)
+	{
+		if (idx < 0 || idx >= arrlen)
+		{
+			exit(1);
+		}
+		return arr2[idx];
+	}
+};
+
+
+
+int main(void)
+{
+	BoundedArray arr(3);
+
+	//arr[0] = Point(3, 4);//저장의 대상이 객체라면, 대입연산자를 사용해야 한다.
+	//Point pt(1, 2);
+	//arr[1] = pt;
+	//cout << arr[0];
+	//cout << arr[1];
+
+	arr[0] = new Point(1, 2);
+	cout << *arr[0];
+	cout << endl;
+	return 0;
+}
+/*
+
+*/
--- Project2/Project2/test.cpp
+++ Project2/Project2/test.cpp
@@ -4,59 +4,34 @@
 using namespace std;
 using std::cin;
 
-class Point
+class BoundedArray
 {
 private:
-	int xpos, ypos;
+	int * arr;
+	int ylen;
 public:
-	Point(int x=0, int y=0) :xpos(x), ypos(y) {} //매개변수의 default값이 없으면 new Point[len]을 해줄 수 없다.
-	//왜냐하면 인자를 받지않는 void생성자의 호출을 통해서 배열요소를 이루는 객체가 생성되기 때문이다.
-	//따라서 void생성자가 호출됐을때 xpos와ypos에 값을 넣어줄 default값이 있어야한다.
-	friend ostream& operator<<(ostream& os, const Point& pos);
+	BoundedArray(int len =0)
+		:ylen(len)
+	{
+		arr = new int[len];
+	}
 };
 
-ostream& operator<<(ostream& os, const Point& pos)
-{
-	os << pos.xpos << "," << pos.ypos << endl;
-	return os;
-}
-
-class BoundCheck
+class Bounded2Darray :public BoundedArray
 {
 private:
-	Point* arr;
-	int arrlen;
+	int xlen;
 public:
-	BoundCheck(int len)
-		:arrlen(len)
-	{
-		arr = new Point[len];
-	}
-	Point& operator[](int idx)
-	{
-		if (idx < 0 || idx >= arrlen)
-		{
-			cout << "index out of range" << endl;
-			exit(1);
-		}
-		return arr[idx];
-	}
-	~BoundCheck()
-	{
-		delete[]arr;
-	}
+	Bounded2Darray(int x,int y)
+		:xlen(x),BoundedArray(y)
+	{}
 
 };
 
 int main(void)
 {
-	BoundCheck arr(3);
-	arr[0] = Point(3, 4);//임시객체를 생성하여 배열요소 초기화
-	arr[1] = Point(5, 6);
-	arr[2] = Point(7, 8);
-
-	cout << arr[0] << endl;
-
+	Bounded2Darray array(3, 4);
+	cout<< array[0][0];
 	cout << endl;
 	return 0;
 }
